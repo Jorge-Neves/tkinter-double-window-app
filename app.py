2330366c59aea6a3a47470d4910bffacf9d0575b
Paulo Santos <paulo.jorge.ngs@gmail.com>
2022-12-01 17:39:10 +0000

    Update app.py

--- app.py
+++ app.py
@@ -114,30 +114,31 @@ main.configure(bg='gray')
 def clear_text():
     code.delete(1.0, END)
 
-def runCode():
-    stdoutNew = sys.stdout
+
+def run_code():
+    stdout_new = sys.stdout
     output = sys.stdout = StringIO()
     exec(code.get(1.0, END))
-    sys.stdout = stdoutNew
+    sys.stdout = stdout_new
     tkinter.messagebox.showinfo('Result', output.getvalue())
 
 
 sidebar = Frame(main, width=80, height=500, bg='#212121')
 sidebar.pack(side=RIGHT)
 
-rightbar = Frame(main, width=720, height=500, bg='#424242')
-rightbar.pack(side=LEFT)
+right_bar = Frame(main, width=720, height=500, bg='#424242')
+right_bar.pack(side=LEFT)
 
 
-btnClear = Button(sidebar, text="Clear", command=lambda : clear_text())
+btnClear = Button(sidebar, text="Clear", command=lambda: clear_text())
 btnClear.place(x=22, y=5)
 btnClear.configure(bg='gray')
 
-btnRun = Button(sidebar, text="Run", command=lambda : runCode())
+btnRun = Button(sidebar, text="Run", command=lambda: run_code())
 btnRun.place(x=24, y=35)
 btnRun.configure(bg='gray')
 
-code = Text(rightbar, font=('arial', 11), fg='white', bg='#424242')
+code = Text(right_bar, font=('arial', 11), fg='white', bg='#424242')
 code.configure(width=720, height=500)
 code.pack(side=RIGHT)
 
